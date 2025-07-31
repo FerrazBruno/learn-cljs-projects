cc57f995081f0d192b4c8b816b87c58dd2784989
FerrazBruno <brunohenriqueferraz94@gmail.com>
2025-07-31 15:03:00 -0300

    Contador

---
+++ counter/src/learn_cljs/counter.cljs
@@ -0,0 +1,43 @@
+(ns ^:figwheel-hooks learn-cljs.counter
+  (:require-macros
+    [hiccups.core :as hiccups])
+  (:require
+    [hiccups.runtime]
+    [goog.dom :as gdom]
+    [goog.events :as gevents]))
+
+(defonce app-state
+  (atom 0))
+
+(def app-container
+  (gdom/getElement "app"))
+
+(defn render
+  [state]
+  (set! (.-innerHTML app-container)
+        (hiccups/html
+          [:div
+           [:p "Counter: " [:strong state]]
+           [:button {:id "up"}   "+"]
+           [:button {:id "down"} "-"]])))
+
+(defonce is-initialized?
+  (do
+    (gevents/listen
+      (gdom/getElement "app")
+      "click"
+      (fn [e]
+        (condp = (aget e "target" "id")
+          "up"   (swap! app-state inc)
+          "down" (swap! app-state dec))))
+
+    (add-watch
+      app-state
+      :counter-observer
+      (fn [key atom old-val new-val]
+        (render new-val)))
+
+    (render @app-state)
+
+    true))
+
